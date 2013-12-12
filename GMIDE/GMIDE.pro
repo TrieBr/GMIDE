2ed31482b66b6ba3012fda0367baa7419fea0aca
Brad Triebwasser <triebr@gmail.com>
2013-12-11 23:04:13 -0700

    Initial files

---
+++ GMIDE/GMIDE.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-12-08T16:20:38
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = GMIDE
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        MainWindow.cpp
+
+HEADERS  += MainWindow.h
+
+FORMS    += mainwindow.ui
+
+RESOURCES = skin.qrc
---
+++ GMIDE/main.cpp
@@ -0,0 +1,30 @@
+#include "MainWindow.h"
+#include <QApplication>
+#include <QStyleFactory>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    a.setStyle(QStyleFactory::create("Fusion"));
+
+    QPalette palette;
+    palette.setColor(QPalette::Window, QColor(53,53,53));
+    palette.setColor(QPalette::WindowText, Qt::white);
+    palette.setColor(QPalette::Base, QColor(40,40,40));
+    palette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+    palette.setColor(QPalette::ToolTipBase, Qt::white);
+    palette.setColor(QPalette::ToolTipText, Qt::white);
+    palette.setColor(QPalette::Text, Qt::white);
+    palette.setColor(QPalette::Button, QColor(53,53,53));
+    palette.setColor(QPalette::ButtonText, Qt::white);
+    palette.setColor(QPalette::BrightText, Qt::red);
+
+    palette.setColor(QPalette::Highlight, QColor(45, 130, 195).lighter());
+    palette.setColor(QPalette::HighlightedText, Qt::black);
+    qApp->setPalette(palette);
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
